b1eed7e71742afb82850dbeea69c60d9152e0a84
Рома Байтеряков <randomsearch.com@gmail.com>
2021-04-22 10:03:06 +0500

    Ver 1.5

--- MyEF6Db/Class1.cs
+++ MyEF6Db/Class1.cs
@@ -6,8 +6,43 @@ using System.Collections.Generic;
 
 namespace MyEF6Db
 {
+    //class MyContextInitializer : DropCreateDatabaseIfModelChanges<ParkContext>
+    //{
+    //    protected override void Seed(ParkContext db)
+    //    {
+    //        Motor m1 = new Motor { Name = "Бензин" };
+    //        Motor m2 = new Motor { Name = "Дизель" };
+
+    //        db.Motors.Add(m1);
+    //        db.Motors.Add(m2);
+
+    //        TypeJob t1 = new TypeJob { Name = "Расточка блока", Motor = m1};
+    //        TypeJob t2 = new TypeJob { Name = "Замена поршней (дизель)", Motor=m2 };
+
+    //        db.TypeJobs.Add(t1);
+    //        db.TypeJobs.Add(t2);
+
+    //        Auto p1 = new Auto { Mark = "Toyta", Model = "Delica", Motor=m1 };
+    //        Auto p2 = new Auto { Mark = "Lada", Model = "Niva", Motor = m2 };
+
+    //        db.Autos.Add(p1);
+    //        db.Autos.Add(p2);
+
+    //        Job j1 = new Job { TypeJob = t1, Date = "23.01.21 12:23:23" , Auto= p1 };
+    //        Job j2 = new Job { TypeJob = t2, Date = "26.01.21 12:23:23", Auto = p2 };
+
+    //        db.Jobs.Add(j1);
+    //        db.Jobs.Add(j2);
+
+    //        db.SaveChanges();
+    //    }
+    //}
     public class ParkContext : DbContext
     {
+        //static ParkContext()
+        //{
+        //    Database.SetInitializer<ParkContext>(new MyContextInitializer());
+        //}
         public ParkContext()
             : base("ParkDB")
         { }
@@ -22,11 +57,8 @@ namespace MyEF6Db
     {
         public int Id { get; set; }//Ид
         public string Mark { get; set; }//Марка
-        //[MaxLength(1000)]
         public string Model { get; set; }//Модель
-       // [ForeignKey("Motor")]
         public int? MotorId { get; set; }
-
         public virtual Motor Motor { get; set; }//Мотор машины
         public virtual ICollection<Job> Job { get; set; }//Список работ с машиной
 
@@ -38,7 +70,6 @@ namespace MyEF6Db
     {
         //[ForeignKey("Auto")]
         public int Id { get; set; }//Ид
-        //[MaxLength(1000)]
         public string Name { get; set; }//Тип мотора
         public virtual ICollection<Auto> Auto { get; set; }//У каких машин
         public virtual ICollection<TypeJob> TypeJob { get; set; }//Какие работы для этого мотора
@@ -49,6 +80,7 @@ namespace MyEF6Db
     {
         public int Id { get; set; }//Ид
         public int? MotorId { get; set; }//Для какого типа мотора
+        //public virtual Motor Motor { get; set; }//Мотор машины (для инициализации)
         public string Name { get; set; }//Название работы
 
         public virtual ICollection<Job> Job { get; set; }//Работы с машинами
@@ -59,6 +91,7 @@ namespace MyEF6Db
     {
         public int Id { get; set; }//Ид
         public int? AutoId { get; set; }//Номер машины
+        //public virtual Auto Auto { get; set; }//Авто (для инициализации)
         public int? TypeJobId{ get; set; }//Номер работы
         public virtual TypeJob TypeJob { get; set; }//Ссылка тип работы
         public string Date { get; set; }//Марка
